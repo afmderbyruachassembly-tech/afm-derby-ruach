c94bd7615322660e40f9e747b26ac1329bfc5d5b
SimzMapisa <simbarashe795@gmail.com>
2025-07-22 07:50:51 +0100

    optimize images and use webp

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,10 +1,11 @@
-import { Bounded } from '@/app/components/bounded';
-import { Content } from '@prismicio/client';
-import { PrismicNextImage } from '@prismicio/next';
-import { PrismicRichText, SliceComponentProps } from '@prismicio/react';
-import clsx from 'clsx';
-import { FC } from 'react';
-import { HiOutlineArrowLongDown } from 'react-icons/hi2';
+import { Bounded } from "@/app/components/bounded";
+import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import clsx from "clsx";
+import Image from "next/image";
+import { FC } from "react";
+import { HiOutlineArrowLongDown } from "react-icons/hi2";
 
 /**
  * Props for `Hero`.
@@ -15,78 +16,87 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero: FC<HeroProps> = ({ slice }) => {
-	console.log(slice.variation);
-	return (
-		<section
-			data-slice-type={slice.slice_type}
-			data-slice-variation={slice.variation}
-			className='relative   md:h-screen xl:h-[90vh] overflow-hidden'>
-			<div className='absolute w-full h-full -z-10'>
-				{slice.variation !== 'default' ? (
-					<div className='tint inset-0 bg-blue-950 w-full h-full backdrop-blur-3xl'>
-						<div className="texture absolute bg-[url('/noisy-background.jpg')] mix-blend-multiply object-contain backdrop-blur-3xl inset-0" />
-					</div>
-				) : null}
+  console.log(slice.variation);
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="relative overflow-hidden md:h-screen xl:h-[90vh]"
+    >
+      <div className="absolute -z-10 h-full w-full">
+        {slice.variation !== "default" ? (
+          <div className="tint inset-0 h-full w-full bg-blue-950 backdrop-blur-3xl">
+            <div className="texture absolute inset-0 bg-[url('/noisy-background.webp')] object-contain mix-blend-multiply backdrop-blur-3xl" />
+          </div>
+        ) : null}
 
-				<div
-					className={clsx(
-						'scale-200 absolute inset-0 w-full h-full',
-						slice.variation !== 'default' ? 'opacity-10' : 'opacity-100'
-					)}>
-					<PrismicNextImage
-						field={slice.primary.bgimage}
-						fill
-						className='object-cover'
-						priority={true}
-					/>
-				</div>
-			</div>
+        <div
+          className={clsx(
+            "absolute inset-0 h-full w-full scale-200",
+            slice.variation !== "default" ? "opacity-10" : "opacity-100",
+          )}
+        >
+          <PrismicNextImage
+            field={slice.primary.bgimage}
+            fill
+            className="object-cover"
+            priority
+          />
+        </div>
+      </div>
 
-			<Bounded className='relative h-full'>
-				<div className='relative text-white rounded-2xl max-w-[70%] h-full flex flex-col items-center justify-center  mx-auto overflow-hidden '>
-					<div className='mix-blend-hard-light'>
-						{/*********Gradient********/}
-						{/*<div className="absolute inset-0 bg-[conic-gradient(from_45deg,_#265D97,_#265D97,_#E33738,_#E33738,_#265D97,_#265D97,_#265D97,_#265D97)] backdrop-blur-3xl" />*/}
+      <Bounded className="relative h-full">
+        <div className="relative mx-auto flex h-full max-w-[70%] flex-col items-center justify-center overflow-hidden rounded-2xl text-white">
+          <div className="mix-blend-hard-light">
+            {/*********Gradient********/}
+            {/*<div className="absolute inset-0 bg-[conic-gradient(from_45deg,_#265D97,_#265D97,_#E33738,_#E33738,_#265D97,_#265D97,_#265D97,_#265D97)] backdrop-blur-3xl" />*/}
 
-						{/******Texture*******/}
-						<div className="texture absolute bg-[url('/noisy-background.jpg')] mix-blend-multiply object-contain backdrop-blur-3xl inset-0" />
+            {/******Texture*******/}
+            <div className="texture absolute inset-0 mix-blend-multiply backdrop-blur-3xl">
+              <Image
+                src="/noisy-background.webp"
+                alt=""
+                fill
+                priority
+                sizes="100vw"
+                style={{ objectFit: "cover" }}
+              />
+            </div>
 
-						<div className="absolute texture h-full  bg-[url('/grainyBg.png')] bg-no-repeat bg-cover backdrop-blur-3xl inset-0 scale-125" />
+            <div className="texture absolute inset-0 h-full scale-125 bg-[url('/grainyBg.webp')] bg-cover bg-no-repeat backdrop-blur-3xl"></div>
+          </div>
 
-						{/*<div className="texture absolute bg-[url('/noisy-background.jpg')] mix-blend-multiply object-contain backdrop-blur-3xl inset-0" />*/}
-					</div>
-
-					{/*******Content Container********/}
-					<div className='relative z-10 px-8 text-center max-w-2xl'>
-						<h1 className='text-6xl font-bold tracking-tighter text-balance text-shadow-md text-shadow-black/20'>
-							<PrismicRichText field={slice.primary.heading} />
-						</h1>
-						<p className='text-xl text-balance text-afm-lightgray text-shadow-md text-shadow-black/20'>
-							<PrismicRichText field={slice.primary.body} />
-						</p>
-						<div className='flex justify-evenly mt-8 text-left'>
-							{slice.primary.location.map((item, index) => (
-								// Render the item
-								<div key={index}>
-									<span className='font-bold text-xl text-afm-gold capitalize'>
-										{' '}
-										<PrismicRichText field={item.leading} />
-									</span>
-									<PrismicRichText field={item.body} />
-								</div>
-							))}
-						</div>
-						<div className='text-center flex flex-col gap-4 items-center justify-center mt-10 animate-[var(--animate-bounce)] '>
-							<span className='origin-center rotate-90'>scroll</span>
-							<span className='text-5xl'>
-								<HiOutlineArrowLongDown />
-							</span>
-						</div>
-					</div>
-				</div>
-			</Bounded>
-		</section>
-	);
+          {/*******Content Container********/}
+          <div className="relative z-10 max-w-2xl px-8 text-center">
+            <div className="text-6xl font-bold tracking-tighter text-balance">
+              <PrismicRichText field={slice.primary.heading} />
+            </div>
+            <div className="text-afm-lightgray text-xl text-balance text-shadow-black/20 text-shadow-md">
+              <PrismicRichText field={slice.primary.body} />
+            </div>
+            <div className="mt-8 flex justify-evenly text-left">
+              {slice.primary.location.map((item, index) => (
+                // Render the item
+                <div key={index}>
+                  <span className="text-afm-gold text-xl font-bold capitalize">
+                    {" "}
+                    <PrismicRichText field={item.leading} />
+                  </span>
+                  <PrismicRichText field={item.body} />
+                </div>
+              ))}
+            </div>
+            <div className="mt-10 flex animate-[var(--animate-bounce)] flex-col items-center justify-center gap-4 text-center">
+              <span className="origin-center rotate-90">scroll</span>
+              <span className="text-5xl">
+                <HiOutlineArrowLongDown />
+              </span>
+            </div>
+          </div>
+        </div>
+      </Bounded>
+    </section>
+  );
 };
 
 export default Hero;
