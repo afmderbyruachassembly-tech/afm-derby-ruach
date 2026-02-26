48cc5d1ef51c5b6bd35f133db6017f4d0157da61
SimzMapisa <simbarashe795@gmail.com>
2026-02-26 04:40:05 +0000

    Add meta tags for SEO optimization in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,12 @@ export default async function RootLayout({
     <html lang="en" className={inter.className}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="AFM Derby Ruach is a church that believes in the power of the Holy Spirit and the Bible." />
+        <meta name="keywords" content="AFM Derby Ruach Assembly, AFM, Church, Bible, Jesus Christ, Gospel" />
+        <meta name="author" content="AFM Derby Ruach Assembly" />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="google" content="notranslate" />
       </head>
       <body>
         <Navbar data={data.navigation.data} />
