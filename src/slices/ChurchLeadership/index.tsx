7f0e66081078eabbbfcaa798248e8238f1a53d19
SimzMapisa <simbarashe795@gmail.com>
2026-02-27 14:49:52 +0000

    Update ChurchLeadership component to improve layout padding and grid responsiveness

--- src/slices/ChurchLeadership/index.tsx
+++ src/slices/ChurchLeadership/index.tsx
@@ -18,7 +18,7 @@ const ChurchLeadership: FC<ChurchLeadershipProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className='py-10'
+      className='py-10 px-4 '
     >
       <Bounded>
         <div className='m-auto max-w-4xl text-4xl leading-[115%] font-black tracking-tighter text-balance capitalize lg:text-center xl:text-6xl'>{slice.primary.title}</div>
@@ -27,7 +27,7 @@ const ChurchLeadership: FC<ChurchLeadershipProps> = ({ slice }) => {
           <div className='flex gap-4 [--corner-radius:16px] grid grid-cols-12'>
             
           {slice.primary.card.map((item) => (
-            <div key={item.name} className='bentocard border-afm-card-bg before:bg-afm-card-bg relative rounded-[var(--corner-radius)] border p-2 before:absolute before:inset-0 before:-z-10 before:m-[calc(var(--corner-radius)/2)] before:rounded-[calc(var(--corner-radius)/2)] before:content-[""]  col-span-3 sm:col-span-6 lg:col-span-4'>
+            <div key={item.name} className='bentocard border-afm-card-bg before:bg-afm-card-bg relative rounded-[var(--corner-radius)] border p-2 before:absolute before:inset-0 before:-z-10 before:m-[calc(var(--corner-radius)/2)] before:rounded-[calc(var(--corner-radius)/2)] before:content-[""]  xl:col-span-3 sm:col-span-6 lg:col-span-4'>
               <PrismicNextImage field={item.profile} className='rounded-sm'/>
               <div className='px-4 pb-2'>
                 <p className='mt-2 font-bold'>{item.name}</p>
