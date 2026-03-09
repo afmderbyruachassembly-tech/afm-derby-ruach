c9c8d6974eeefd9f933f92d09f113ad50701fbfa
SimzMapisa <simbarashe795@gmail.com>
2026-03-09 10:05:50 +0000

    feat: update site metadata and add web app manifest
    - Removed old icon file.
    - Enhanced layout.tsx with additional meta tags for SEO and updated favicon links.
    - Added site.webmanifest for PWA support with app icons and display settings.
    - Introduced new web app icons for better visual representation on devices.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,17 +56,43 @@ export default async function RootLayout({
     <html lang="en" className={inter.className}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="AFM Derby Ruach is a church that believes in the power of the Holy Spirit and the Bible." />
-        <meta name="keywords" content="AFM Derby Ruach Assembly, AFM, Church, Bible, Jesus Christ, Gospel" />
+        <meta
+          name="description"
+          content="AFM Derby Ruach is a church that believes in the power of the Holy Spirit and the Bible."
+        />
+        <meta
+          name="keywords"
+          content="AFM Derby Ruach Assembly, AFM, Church, Bible, Jesus Christ, Gospel"
+        />
         <meta name="author" content="AFM Derby Ruach Assembly" />
         <meta name="robots" content="index, follow" />
         <meta name="googlebot" content="index, follow" />
         <meta name="google" content="notranslate" />
-        <script async defer src="https://static.cdn.prismic.io/prismic.js?new=true&repo=afmderby"></script>
+
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Afm Derby" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <script
+          async
+          defer
+          src="https://static.cdn.prismic.io/prismic.js?new=true&repo=afmderby"
+        ></script>
       </head>
       <body>
         <Navbar data={data.navigation.data} />
-        <div className="relative min-h-[100vh] ">{children}</div>
+        <div className="relative min-h-[100vh]">{children}</div>
       </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
