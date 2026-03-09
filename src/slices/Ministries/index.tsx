33f8d15787743509f302525110e38ffa63432d59
SimzMapisa <simbarashe795@gmail.com>
2026-03-09 10:06:45 +0000

    refactor: standardize import quotes and improve layout structure in Ministries component for better responsiveness

--- src/slices/Ministries/index.tsx
+++ src/slices/Ministries/index.tsx
@@ -1,6 +1,6 @@
-import { Bounded } from '@/components/bounded';
+import { Bounded } from "@/components/bounded";
 import { Content } from "@prismicio/client";
-import { PrismicNextImage } from '@prismicio/next';
+import { PrismicNextImage } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { FC } from "react";
 
@@ -18,20 +18,32 @@ const Ministries: FC<MinistriesProps> = ({ slice }) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <Bounded>
-        <div className='py-40'>
+      <Bounded className="px-4 py-10">
+        <div className="md:pt-32">
           {slice.primary.ministrycard.map((item) => (
-            <div key={item.ministry_name} className='relative mb-40 grid grid-cols-12 gap-4'>
-              <div className='col-span-6'>
-                 <h1 className="text-8xl font-extrabold opacity-3 absolute z-0 -top-10 max-w-sm">{item.ministry_name}</h1>
-              <h3 className="text-6xl font-extrabold mb-4">{item.ministry_name}</h3>
-              <div><PrismicRichText field={item.description} /></div>
+            <div
+              key={item.ministry_name}
+              className="relative mb-20 gap-4 md:mb-40 md:grid md:grid-cols-12"
+            >
+              <div className="mb-4 md:col-span-6 md:mb-0">
+                <h1 className="absolute -top-10 z-0 hidden max-w-sm font-extrabold opacity-3 sm:inline-block md:text-8xl">
+                  {item.ministry_name}
+                </h1>
+                <h3 className="mb-4 text-3xl font-extrabold md:text-6xl">
+                  {item.ministry_name}
+                </h3>
+                <div>
+                  <PrismicRichText field={item.description} />
+                </div>
               </div>
-              <div className='col-span-6'>
-                <PrismicNextImage field={item.ministry_image} width={300} />
+              <div className="md:col-span-6">
+                <PrismicNextImage
+                  field={item.ministry_image}
+                  width={300}
+                  className="w-full"
+                />
               </div>
             </div>
-            
           ))}
         </div>
       </Bounded>
